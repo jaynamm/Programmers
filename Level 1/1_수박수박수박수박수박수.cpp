3dd9841c64cc8d7091b485cd4620d83c350941f4
jaynamm <48994100+jaynamm@users.noreply.github.com>
2019-09-01 18:50:50 +0900

    Update 1_수박수박수박수박수박수.cpp

--- Level 1/1_수박수박수박수박수박수.cpp
+++ Level 1/1_수박수박수박수박수박수.cpp
@@ -7,8 +7,8 @@ string solution(int n) {
     string answer = "";
     
     for(int i=1; i<=n; i++){
-        if(i%2 == 1) answer += "¼ö";
-        else if(i%2 == 0) answer += "¹Ú";
+        if(i%2 == 1) answer += "ìˆ˜";
+        else if(i%2 == 0) answer += "";
     }
     
     return answer;
