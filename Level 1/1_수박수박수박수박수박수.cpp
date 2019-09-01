848294dedf839813acce03d9fbc0432a0b279e56
jaynamm <48994100+jaynamm@users.noreply.github.com>
2019-09-01 18:50:57 +0900

    Update 1_수박수박수박수박수박수.cpp

--- Level 1/1_수박수박수박수박수박수.cpp
+++ Level 1/1_수박수박수박수박수박수.cpp
@@ -8,7 +8,7 @@ string solution(int n) {
     
     for(int i=1; i<=n; i++){
         if(i%2 == 1) answer += "수";
-        else if(i%2 == 0) answer += "";
+        else if(i%2 == 0) answer += "박";
     }
     
     return answer;
